30ed22765f734b37f4eda5c3e62669e5174498f1
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-06-21 03:56:47 +0600

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,10 @@
-n = int(input().strip())
-if 1<=n<=150:
-    for i in range(1, (n+1)):
-            
-            #print(i, end = '')
-            #print(i, end = '\n')  
-            print(i, end = '@nonewlineconcate')    
+import math
+AB = float(input().strip())
+BC = float(input().strip())
+
+if 0<AB<=100 and 0<BC<=100:
+    x = math.degrees(math.atan(AB/BC)) #inverse of tan (opposite/base)
+    print(int(x) , chr(176), sep= '')
+    #print(f'{round(x)}{chr(176)}') #character that represents the unicode 176 as (angle/degree)
 else: 
-    print("Out of the range of 1 to 20")
+    print("Out of the range of 1 to 100")
