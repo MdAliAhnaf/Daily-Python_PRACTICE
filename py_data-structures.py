4164b26822853169e5dbba2224f93e0ab310a65c
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-06 22:58:14 +0600

    up

--- py_data-structures.py
+++ py_data-structures.py
@@ -3,4 +3,11 @@
 #Lists are mutable
 #can hold on to different data types
 
+list = ['hi' , 'bye' , 0, 9]
 
+print(list)
+
+# <<TUPLE>>
+#ordered list of values
+#immutable
+#in many ways, it is simillar to a list; one of the most important difference is that tuples can be used as KEYs in a dictionary or elements of a SET.
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-a=5
-print(f'a is {a}') # Preferred method for cvpr.
-print('a is {}'.format(a))
-print('a is ' + str(a))
-print('a is %d %d' % (a, a))
+list = ['hi' , 'bye' , 0, 9]
+
+print(list)
