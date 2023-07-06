72717299b0f8da916148cd2113971d52d808f64d
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-07 00:31:34 +0600

    Update py_data-structures.py

--- py_data-structures.py
+++ py_data-structures.py
@@ -3,11 +3,26 @@
 #Lists are mutable
 #can hold on to different data types
 
-list = ['hi' , 'bye' , 0, 9]
+list = ['hi' , 'bye' , 0, 9, 1, 2]
 
 print(list)
+print(list[2], list[1], list[-3]) #0, 1, 2, .... n [index] [-n] -> -1, - 2, .... -n (counting from backwards)
+
+print(end = '\n')
+
+for i,v in enumerate(list):
+    print(f"{i}:{v}")
+
+print(end = '\n')
 
 # <<TUPLE>>
 #ordered list of values
 #immutable
 #in many ways, it is simillar to a list; one of the most important difference is that tuples can be used as KEYs in a dictionary or elements of a SET.
+
+tupl = ('morning!', 'night' , 99 , 100)
+for i in tupl:
+    print(i)
+print(end = '\n')
+print(tupl[-1])
+
