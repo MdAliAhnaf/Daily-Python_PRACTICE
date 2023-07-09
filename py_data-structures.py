babbf2e51bba77e93e34cf53a1fc108307a1d1b6
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-09 19:25:48 +0600

    up

--- py_data-structures.py
+++ py_data-structures.py
@@ -238,8 +238,7 @@ print(x) #after the change
 print(y)
 print(end = '\n')
 
-
-z = car.items()
+z = car.items() #complete dictionary items
 print(z)
 if "color" in car:
   print("Yes, 'color' is one of the keys in the car dictionary")
@@ -255,8 +254,22 @@ car.popitem()
 print(car)
 del car["model"]
 print(car)
+car.clear() # clear() method empties the dictionary
+print(car)
 del car # del keyword can  delete the dictionary completely
 
 thisdict = dict(name = "John", age = 36, country = "Norway")
 print(thisdict)
 x = thisdict.get("country")
+print(x)
+
+d = {'person': 2, 'cat': 4, 'dog': 4, 'spider': 8}
+for i in d: 
+    print(i) #keys 
+print(end = '\n')
+for i in d:
+    y = d[i] #from keys takes paired values <<only values>>
+    print('A %s has %d legs' % (i, y))
+print(end = '\n')
+for i,y in d.items():
+    print('A %s has %d legs' % (i, y)) 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,38 +1,15 @@
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(car, type(car))
-print("Paired key's value of the key BRAND: " + car["brand"]) #prints key's value as pairs
-print(end = '\n')
-x = car.keys()
-y = car.values()
-print(x) #before the change
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+x = thisdict.get("country")
 print(x)
 
-car["color"] = "white"
-car["color"] = "black"
-car["year"] = "2022"
-print(x) #after the change
-print(y)
+d = {'person': 2, 'cat': 4, 'dog': 4, 'spider': 8}
+for i in d: 
+    print(i) #keys 
 print(end = '\n')
-
-
-z = car.items()
-print(z)
-if "color" in car:
-  print("Yes, 'color' is one of the keys in the car dictionary")
-else:
-   print("ERROR")
+for i in d:
+    y = d[i] #from keys takes paired values <<only values>>
+    print('A %s has %d legs' % (i, y))
 print(end = '\n')
-
-car.update({"year": 2023})
-print(car)
-print(end = '\n')
-# popitem() method removes the <<last inserted>> item (in versions before 3.7, a random item is removed instead)
-car.popitem()
-print(car)
-del car["model"]
-print(car)
-del car # del keyword can  delete the dictionary completely
+for i,y in d.items():
+    print('A %s has %d legs' % (i, y)) 
