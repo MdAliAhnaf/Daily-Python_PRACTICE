1736b6796171943e28aff43840e91d9495ee2c31
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-09 17:24:37 +0600

    up

--- py_data-structures.py
+++ py_data-structures.py
@@ -207,4 +207,6 @@ a = set(["Jake", "John", "Eric"])
 b = set(["John", "Jill"])
 
 print(a.intersection(b)) #takes the common
-print(a.symmetric_difference(b))   #attended only one of the events / non-common
+print(a.symmetric_difference(b))  #attended only one of the events / non-common
+print(a.difference(b)) #members attended only one event and not the other
+print(a.union(b)) #list of all participants 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,4 +2,5 @@ a = set(["Jake", "John", "Eric"])
 b = set(["John", "Jill"])
 
 print(a.intersection(b)) #takes the common
-print(a.symmetric_difference(b))
+print(a.symmetric_difference(b))  #attended only one of the events / non-common
+print(a.difference(b)) #members attended only one event and not the other
