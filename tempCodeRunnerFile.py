82cf609102d6687f4bd6b05823477795d2cdebf6
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-12 19:54:29 +0600

    up

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-  def __str__(self):                     # The string representation of an object WITH the __str__() function
-    return f"NAME:{self.name} AGE:({self.age})"
-
-myobjectp1 = Person("John", 36)
-
-print(myobjectp1)
+import numpy as np
